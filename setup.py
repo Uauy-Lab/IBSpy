919d06a45d7446a318ce6df4f375aaf33507d7b0
Ricardo H. Ramirez-Gonzalez <ricardo.ramirez-gonzalez@jic.ac.uk>
2020-12-22 20:09:03 -0600

    Renamed library folder. More tests on the pip and setup files

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="ibspy-homonecloco", # Replace with your own username
+    name="ibspy", # Replace with your own username
     version="0.0.1",
     author="Ricardo H. Ramirez-Gonzalez",
     author_email="ricardo.ramirez-gonzalez@jic.ac.ul",
