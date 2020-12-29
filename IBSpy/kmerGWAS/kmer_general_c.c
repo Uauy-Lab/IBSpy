1b02c5c6dfe85b9093385554d56a991fa9e972a5
Luca Venturini <luca.venturini@earlham.ac.uk>
2020-12-29 10:19:40 +0000

    Restored the original C file. We need to separate the names of the C code and the Python code, otherwise Cython will overwrite the C sources.

---
+++ IBSpy/kmerGWAS/kmer_general_c.c
@@ -0,0 +1,51 @@
+#include <assert.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+#include "kmer_general.h"
+#include "nucleotide.h"
+
+
+/**
+ * @brief  Given a k-mer representation in bits convert it to string
+ * @param  64-bit k-mer representation 
+ * @return  string with bp representation
+ */
+void bits2kmer31(kmerGWAS_kmer w, const uint8_t kmer_size, char * res) {
+	const static char dict_bp[] = {'A','C','G','T'};
+	const static kmerGWAS_kmer mask2bits = 0x0000000000000003;
+ 	uint8_t i;
+	for(i=0; i<kmer_size; i++) {
+		res[kmer_size-i-1] = dict_bp[w & mask2bits];
+		w = (w>>2);
+	}
+	res[kmer_size] = '\0';
+	return;
+}
+
+char * kmer_string_alloc(const uint8_t kmer_size){
+	char* res = malloc(sizeof(char) * (kmer_size+1));
+	assert(res != NULL);
+	return res;
+}
+
+void kmer_string_free(char * res){
+	free(res);
+}
+
+kmerGWAS_kmer kmer2bits(char * kmer, uint8_t kmer_size) {
+	kmerGWAS_kmer b  = 0;
+	kmerGWAS_kmer bt = 0;
+	//kmerGWAS_kmer cur_dubit;
+	for(size_t i=0; i < kmer_size; i++) {
+		Nucleotide n = char_to_binary_nucleotide(kmer[kmer_size - i - 1]);
+		
+		b |= (n << (i*2));
+	}
+	//kmerGWAS_kmer bt = kmer2bits(b, k.size());  
+	bt = kmer_reverse_complement(b,  kmer_size);
+	if (bt < b)
+		return bt;
+	else
+		return b;
+}
