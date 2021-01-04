3db313fbbb339ed3a79c186cb79b99274ae31e25
Ricardo H. Ramirez-Gonzalez <ricardo.ramirez-gonzalez@jic.ac.uk>
2021-01-04 16:52:50 -0600

    Reformatted with black

--- IBSpy/kmerGWAS_kmer.py
+++ IBSpy/kmerGWAS_kmer.py
@@ -1,52 +1,51 @@
 from .kmer_database import KmerDB, KmerBuilder, FastaChunkReader
-#from ctypes import *
+
+# from ctypes import *
 import os
 import inspect
 import kmerGWAS
 import cython
-#from Bio import SeqIO
-#import pysam
+
+# from Bio import SeqIO
+# import pysam
 from pyfaidx import Fasta
 
+
 class KmerGWASDB(KmerDB):
+    def __init__(self, kmer_size):
+        self._builder = KmerGWASDBBuilder(kmer_size)
+        self.kmer_size = kmer_size
 
-	def __init__(self, kmer_size):
-		self._builder = KmerGWASDBBuilder(kmer_size)
-		self.kmer_size = kmer_size
+    def load_from_fasta(self, filename, buffer_size=10000):
+        # fasta = pysam.FastaFile(filename)
+        fasta_iter = FastaChunkReader(
+            filename, chunk_size=buffer_size, kmer_size=self.kmer_size
+        )
+        for chunk in fasta_iter:
+            print(chunk)
+            # max_width = len(ref)
 
-	def load_from_fasta(self, filename, buffer_size=10000):
-		#fasta = pysam.FastaFile(filename)
-		fasta_iter = FastaChunkReader(filename, 
-			chunk_size = buffer_size, 
-			kmer_size  = self.kmer_size)
-		for chunk in fasta_iter:
-			print(chunk)
-			#max_width = len(ref)
+        # print(references)
 
+    # def load_kmers_from_sequence(self, string):
 
-		#print(references)
+    def __len__(self):
+        raise NotImplementedError
 
-	#def load_kmers_from_sequence(self, string):
-
-
-	def __len__(self):
-		raise NotImplementedError
-
-	def __contains__(self, key):
-		raise NotImplementedError
+    def __contains__(self, key):
+        raise NotImplementedError
 
 
 class KmerGWASDBBuilder(KmerBuilder):
-	#cdef char* _builder
-	def __init__(self, kmer_size):
-		self.kmer_size = kmer_size
-		self._builder = kmerGWAS.KmerGWAS_builder(kmer_size)
-		#print(self._builder);
+    # cdef char* _builder
+    def __init__(self, kmer_size):
+        self.kmer_size = kmer_size
+        self._builder = kmerGWAS.KmerGWAS_builder(kmer_size)
+        # print(self._builder);
 
-	def string_to_kmer(self, sequence):
-		binary_kmer = self._builder.string_to_kmer(sequence)
-		return binary_kmer
+    def string_to_kmer(self, sequence):
+        binary_kmer = self._builder.string_to_kmer(sequence)
+        return binary_kmer
 
-
-	def kmer_to_string(self, binary_kmer):
-		return self._builder.kmer_to_string(binary_kmer).decode('UTF-8')
+    def kmer_to_string(self, binary_kmer):
+        return self._builder.kmer_to_string(binary_kmer).decode("UTF-8")
--- IBSpy/kmer_database.py
+++ IBSpy/kmer_database.py
@@ -1,73 +1,73 @@
 from abc import ABC, abstractmethod
 from pyfaidx import Fasta
 
-class KmerDB(ABC):
-	@abstractmethod
-	def __contains__(self, key):
-		raise NotImplementedError
 
-	@abstractmethod
-	def	__len__(self):
-		raise NotImplementedError
+class KmerDB(ABC):
+    @abstractmethod
+    def __contains__(self, key):
+        raise NotImplementedError
+
+    @abstractmethod
+    def __len__(self):
+        raise NotImplementedError
+
 
 class KmerBuilder(ABC):
+    def __init__(self, kmer_size):
+        self.kmer_size = kmer_size
 
-	def __init__(self, kmer_size):
-		self.kmer_size = kmer_size
+    @abstractmethod
+    def string_to_kmer(self, sequence):
+        raise NotImplementedError
 
-	@abstractmethod
-	def string_to_kmer(self,sequence):
-		raise NotImplementedError
+    @abstractmethod
+    def kmer_to_string(self, sequence):
+        raise NotImplementedError
 
-	@abstractmethod
-	def kmer_to_string(self,sequence):
-		raise NotImplementedError
-
-	def sequence_to_kmers(self, sequence, filter_ambiguity = True):
-		ret = [None] * (len(sequence) - self.kmer_size + 1)
-		sequence = sequence.upper()
-		for start in range(0, len(sequence) - self.kmer_size + 1):
-			end = start + self.kmer_size
-			kmer = sequence[start:end]
-			count =  kmer.count('A')
-			count += kmer.count('T')
-			count += kmer.count('G')
-			count += kmer.count('C') 
-			if filter_ambiguity and count != self.kmer_size:
-				continue 
-			ret[start] = kmer
-		return  [i for i in ret if i] 
+    def sequence_to_kmers(self, sequence, filter_ambiguity=True):
+        ret = [None] * (len(sequence) - self.kmer_size + 1)
+        sequence = sequence.upper()
+        for start in range(0, len(sequence) - self.kmer_size + 1):
+            end = start + self.kmer_size
+            kmer = sequence[start:end]
+            count = kmer.count("A")
+            count += kmer.count("T")
+            count += kmer.count("G")
+            count += kmer.count("C")
+            if filter_ambiguity and count != self.kmer_size:
+                continue
+            ret[start] = kmer
+        return [i for i in ret if i]
 
 
 class FastaChunkReader:
-	
-	def __init__(self, filename, chunk_size=10000 , kmer_size=31):
-		self.fasta = Fasta(filename)
-		self.current_ref   = 0
-		self.current_start = 0
-		self.chunk_size    = chunk_size
-		self.kmer_size     = kmer_size
-		self.seqnames      = list(self.fasta.keys())
+    def __init__(self, filename, chunk_size=10000, kmer_size=31):
+        self.fasta = Fasta(filename)
+        self.current_ref = 0
+        self.current_start = 0
+        self.chunk_size = chunk_size
+        self.kmer_size = kmer_size
+        self.seqnames = list(self.fasta.keys())
 
-	def __iter__(self):
-		return self
+    def __iter__(self):
+        return self
 
-	def __next__(self):
-		if(len(self.seqnames) == self.current_ref):
-			raise StopIteration
-		#print(self.seqnames)
-		#print(self.current_ref)
-		seqname = self.seqnames[self.current_ref]
-		start   = self.current_start
-		end    = start + self.chunk_size
-		self.current_start = end - self.kmer_size
-		if(end >= len(self.fasta[seqname]) ):
-			self.current_start  = 0
-			self.current_ref   += 1
-			end = len(self.fasta[seqname])
-		return {
-		'seqname': seqname,
-		'start'  : start,
-		'end'   : end,
-		'seq': self.fasta[seqname][start:end].seq}
-
+    def __next__(self):
+        if len(self.seqnames) == self.current_ref:
+            raise StopIteration
+        # print(self.seqnames)
+        # print(self.current_ref)
+        seqname = self.seqnames[self.current_ref]
+        start = self.current_start
+        end = start + self.chunk_size
+        self.current_start = end - self.kmer_size
+        if end >= len(self.fasta[seqname]):
+            self.current_start = 0
+            self.current_ref += 1
+            end = len(self.fasta[seqname])
+        return {
+            "seqname": seqname,
+            "start": start,
+            "end": end,
+            "seq": self.fasta[seqname][start:end].seq,
+        }
