b58fda0f2aee420fd649ca4ff494b634eb6e3948
Ricardo H Ramirez-Gonzalez <ricardo.ramirez-gonzalez@jic.ac.uk>
2021-06-18 22:19:10 +0100

    Added script with integration test for python

---
+++ tests/integration_python.sh
@@ -0,0 +1,20 @@
+#/bin/sh
+
+lines="jagger arinalrfor julius lancer landmark mace norin61 spelta stanley sy_mattis"
+
+data_path="./tests/data"
+kmer_db=$data_path/test4B.jagger.kmerGWAS_k31
+expected_output="tests/data/ibspy_integration_test.txt"
+output="tests/ibspy_out.txt"
+
+for l in $lines; do
+	ref="$data_path/test4B.$l.fa"
+	echo $ref
+	IBSpy -d $kmer_db -r $ref -k 31 -w 3000 -f kmerGWAS
+done > $output
+
+def_ret=`diff $output $expected_output`
+echo $def_ret
+echo ${#def_ret}
+exit ${#def_ret}
+
