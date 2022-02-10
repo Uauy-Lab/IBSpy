c1f788e4632b9420befbb862767206d30657bc3a
Ricardo H Ramirez-Gonzalez <ricardo.ramirez-gonzalez@jic.ac.uk>
2022-02-10 14:34:25 +0000

    Added unit tests for the affinity prpagation over several iterations

--- IBSpy/window_affinity_propagation.py
+++ IBSpy/window_affinity_propagation.py
@@ -1,35 +1,42 @@
+from cmath import nan
+import math
 import sys
+import warnings
 from sklearn.cluster import AffinityPropagation
+from sklearn.exceptions import ConvergenceWarning
 from sklearn.preprocessing import StandardScaler
 from sklearn import metrics
 import random
 
-
 def single_affinity_run(x, random_state=42, damping=0.5):
 	af = AffinityPropagation(random_state=random_state, damping=damping).fit(x)
 	predicted = af.predict(x)
-	if len(af.cluster_centers_indices_) > 2:
-		sc_score = '%0.2f'%metrics.silhouette_score(x, af.labels_, metric='sqeuclidean')
+	centers = len(af.cluster_centers_indices_) 
+	if centers > 2 and centers < len(x):
+		sc_score = metrics.silhouette_score(x, af.labels_, metric='sqeuclidean')
 	else:
-		sc_score = 0
+		sc_score = math.nan
 	return (predicted, sc_score)
 
-
-
-def cluster_by_haplotype (gr, dampings=[0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9], iterations=100, seed=42):
-	t_df = gr.as_df.set_index(['Chromosome', 'Start', 'End']).T
-	x = StandardScaler(with_mean=True).fit_transform(t_df)
-	predicted = list()
-	scores = list()
-	used_dampings = list()
-	random_states = list()
-	random.seed(seed)
-	for i in range(iterations):
+def cluster_by_haplotype (gr, dampings=[0.5,0.6,0.7,0.8,0.9], iterations=100, seed=42):
+	with warnings.catch_warnings():
+		warnings.filterwarnings("ignore", category=ConvergenceWarning)
+		t_df = gr.as_df().set_index(['Chromosome', 'Start', 'End']).T
+		# print(t_df.columns)
+		varieties = t_df.index
+		x = StandardScaler(with_mean=True).fit_transform(t_df)
+		predicted = list()
+		scores = list()
+		used_dampings = list()
+		random_states = list()
+		random.seed(seed)
+		max_seed = 2**32 - 1
 		for dmp in dampings:
-			random_state = random.randint(1, sys.maxsize)
-			X_predicted, sc_score = single_affinity_run(x, random_state=random_state, damping=dmp)
-			predicted.append(X_predicted)
-			scores.append(sc_score)
-			used_dampings.append(dmp)
-			random_states.append(random_state)
-	return predicted, scores, used_dampings, random_states
+			for i in range(iterations):
+				random_state = random.randint(1, max_seed)
+				X_predicted, sc_score = single_affinity_run(x, random_state=random_state, damping=dmp)
+				predicted.append(X_predicted)
+				scores.append(sc_score)
+				used_dampings.append(dmp)
+				random_states.append(random_state)		
+	return predicted, scores, used_dampings, random_states, varieties
--- tests/test_affinity_propagation.py
+++ tests/test_affinity_propagation.py
@@ -1,29 +1,79 @@
+import math
+import os
 import unittest
-import numpy
-
 import pandas as pd
 from pyranges import PyRanges
 from sklearn.preprocessing import StandardScaler
-from IBSpy.window_affinity_propagation import single_affinity_run
+from IBSpy.window_affinity_propagation import single_affinity_run, cluster_by_haplotype
+import numpy as np
+import pandas.testing as pdt
 
+def clustering_to_df(predicted, varieties):
+	df = pd.DataFrame(predicted).T
+	df.insert(loc = 0,
+          column = 'variety',
+          value = varieties)
+	return df
 
 class TestAffinityPropagation(unittest.TestCase):
 	def setUp(self) -> None:
 		self.test_prefix="./tests/data/affinity/windows"
-		pass
-	
+		self.good_region="./tests/data/affinity/good_region_test.tsv"
+		self.bad_region="./tests/data/affinity/bad_region_test.tsv" 
+		self.good_predicted_expected="./tests/data/affinity/predicted_good.tsv"
+		self.bad_predicted_expected="./tests/data/affinity/predicted_bad.tsv" 
+		self.out_folder="./tests/data/affinity/out/"
+		try:
+			os.mkdir(self.out_folder)
+		except OSError as error:
+			pass
+
 	def test_single_affinity_run(self):
 		df = pd.read_csv(self.test_prefix + "/0.tsv", delimiter="\t")
 		gr = PyRanges(df)
-		# df = gr.as_df()
 		t_df = gr.as_df().set_index(['Chromosome', 'Start', 'End']).T
 		x = StandardScaler(with_mean=True).fit_transform(t_df)
 		predicted, score = single_affinity_run(x)
 		expected_predicted=[0,0,1,1]
-		expected_score = 0
-		numpy.testing.assert_array_almost_equal(expected_predicted, predicted)
-		self.assertEqual(expected_score, score)
-		pass
+		# expected_score = math.nan
+		np.testing.assert_array_almost_equal(expected_predicted, predicted)
+		self.assertTrue(math.isnan(score))
+	
+	def test_cluster_by_haplotype_short(self):
+		df = pd.read_csv(self.test_prefix + "/0.tsv", delimiter="\t")
+		gr = PyRanges(df)
+		predicted, scores, used_dampings, random_states, varieties = cluster_by_haplotype(gr, seed=42, iterations=10, dampings=[0.6, 0.7])
+		expected_predicted=[0,0,1,1]
+		expected_predicted2=[0,0,0,0]
+		for i, p in enumerate(predicted):
+			ep = expected_predicted
+			if i == 3:
+				ep = expected_predicted2
+			np.testing.assert_array_almost_equal(ep, p)
+		for s in scores:
+			self.assertTrue(math.isnan(s))
+		# df2 = pd.DataFrame(predicted)
+		# print(df2) 	
+	
+	def test_cluster_by_haplotype_long(self):
+		
+		df = pd.read_csv(self.good_region, delimiter="\t")
+		gr = PyRanges(df)
+		predicted, scores, used_dampings, random_states, varieties = cluster_by_haplotype(gr, seed=42, iterations=3, dampings=[0.5, 0.6, 0.7, 0.8, 0.9])
+		df = clustering_to_df(predicted, varieties)
+		df.to_csv("./tests/data/affinity/out/predicted_good.tsv", sep="\t", index=False)
+		expected=pd.read_csv(self.good_predicted_expected, delimiter="\t")
+		df = pd.read_csv("./tests/data/affinity/out/predicted_good.tsv",delimiter="\t")
+		pdt.assert_frame_equal(expected, df)
+		
+		df = pd.read_csv(self.bad_region, delimiter="\t")
+		gr = PyRanges(df)
+		predicted, scores, used_dampings, random_states, varieties = cluster_by_haplotype(gr, seed=42, iterations=3, dampings=[0.5, 0.6, 0.7, 0.8, 0.9])
+		df = clustering_to_df(predicted, varieties)
+		df.to_csv("./tests/data/affinity/out/predicted_bad.tsv", sep="\t", index=False)
+		expected=pd.read_csv(self.bad_predicted_expected, delimiter="\t")
+		df = pd.read_csv("./tests/data/affinity/out/predicted_bad.tsv",delimiter="\t")
+		pdt.assert_frame_equal(expected, df)
 
 if __name__ == '__main__':
     unittest.main()
