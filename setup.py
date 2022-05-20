92a3df3be652bda92880e9d92dad9057919772de
Ricardo H Ramirez-Gonzalez <ricardo.ramirez-gonzalez@jic.ac.uk>
2022-05-20 10:28:18 +0100

    Updated version

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="IBSpy",
-    version="0.4.9",
+    version="0.4.10",
     author="Ricardo H. Ramirez-Gonzalez",
     author_email="ricardo.ramirez-gonzalez@jic.ac.uk",
     description="A package to detect IBS regions",
